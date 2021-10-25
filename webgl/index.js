5c9ddd660882debc4c92b70e2cf928bc5d529de0
Ashley Feniello <ashleyf@briefrobotics.com>
2021-10-24 20:00:59 -0700

    Beginning tutorial 2 - spinning triangle

--- webgl/index.js
+++ webgl/index.js
@@ -59,19 +59,27 @@ function init() {
     }
 
     function enableAttribute(name, size, offset, cols, gl, program) {
-        var vertPositionLoc = gl.getAttribLocation(program, name);
-        gl.vertexAttribPointer(vertPositionLoc, size, gl.FLOAT, gl.FALSE, cols * Float32Array.BYTES_PER_ELEMENT, offset * Float32Array.BYTES_PER_ELEMENT);
-        gl.enableVertexAttribArray(vertPositionLoc);
+        var loc = gl.getAttribLocation(program, name);
+        gl.vertexAttribPointer(loc, size, gl.FLOAT, gl.FALSE, cols * Float32Array.BYTES_PER_ELEMENT, offset * Float32Array.BYTES_PER_ELEMENT);
+        gl.enableVertexAttribArray(loc);
         return offset + size;
+    }
+
+    function enableUniformMatrix(name, matrix) {
+        var loc = gl.getUniformLocation(program, name);
+        gl.uniformMatrix4fv(loc, gl.FALSE, matrix);
     }
 
     var vertexShaderSource =
         'precision mediump float;' +
-        'attribute vec2 vertPosition;' +
+        'attribute vec3 vertPosition;' +
         'attribute vec3 vertColor;' +
         'varying vec3 fragColor;' +
+        'uniform mat4 matWorld;' +
+        'uniform mat4 matView;' +
+        'uniform mat4 matProj;' +
         'void main() {' +
-        '  gl_Position = vec4(vertPosition, 0.0, 1.0);' +
+        '  gl_Position = matProj * matView * matWorld * vec4(vertPosition, 1.0);' +
         '  fragColor = vertColor;' +
         '}';
 
@@ -86,17 +94,43 @@ function init() {
     var program = initShaders(vertexShaderSource, fragmentShaderSource, gl);
 
     var triangleVertices = new Float32Array([
-         0.0,  0.5,  1.0, 1.0, 0.0,
-        -0.5, -0.5,  0.7, 0.0, 1.0,
-         0.5, -0.5,  0.1, 1.0, 0.6,
+         0.0,  0.5, 0.0,  1.0, 1.0, 0.0,
+        -0.5, -0.5, 0.0,  0.7, 0.0, 1.0,
+         0.5, -0.5, 0.0,  0.1, 1.0, 0.6,
     ]);
 
     loadBuffer(triangleVertices, gl);
 
-    const cols = 5;
+    const cols = 6;
     var offset = 0;
-    offset = enableAttribute("vertPosition", 2, offset, cols, gl, program);
+    offset = enableAttribute("vertPosition", 3, offset, cols, gl, program);
     offset = enableAttribute("vertColor", 3, offset, cols, gl, program);
 
-    gl.drawArrays(gl.TRIANGLES, 0, triangleVertices.length / cols);
+	var worldMatrix = new Float32Array(16);
+	var viewMatrix = new Float32Array(16);
+    var projMatrix = new Float32Array(16);
+
+    glMatrix.mat4.identity(worldMatrix);
+    glMatrix.mat4.lookAt(viewMatrix, [0, 0, -2], [0, 0, 0], [0, 1, 0]);
+	glMatrix.mat4.perspective(projMatrix, Math.PI / 4, 800 / 600, 0.1, 1000.0);
+
+    enableUniformMatrix("matWorld", worldMatrix);
+    enableUniformMatrix("matView", viewMatrix);
+    enableUniformMatrix("matProj", projMatrix);
+
+    var identityMatrix = new Float32Array(16);
+    glMatrix.mat4.identity(identityMatrix);
+
+    var loop = function() {
+        var angle = performance.now() / 1000 / 6 * 2 * Math.PI;
+        glMatrix.mat4.rotate(worldMatrix, identityMatrix, angle, [0, 1, 0]);
+        enableUniformMatrix("matWorld", worldMatrix);
+
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        gl.drawArrays(gl.TRIANGLES, 0, triangleVertices.length / cols);
+
+        requestAnimationFrame(loop);
+    }
+    requestAnimationFrame(loop);
 }
